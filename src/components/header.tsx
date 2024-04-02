99be2a4748262571e7b60f1995fe748126ff3407
Letícia Vieira <lvieira268@gmail.com>
2024-04-02 20:38:12 -0300

    feat: styles header
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
 export function Header() {
   return (
-    <div>
+    <div className="flex items-center gap-5 py-2">
       <img src={nlwUniteIcon} alt="nlw-unite-icon" />
-      <nav>
-        <a href="">Eventos</a>
-        <a href="">Participantes</a>
+      <nav className="flex items-center gap-5">
+        <a className="font-medium text-sm text-zinc-300" href="">
+          Eventos
+        </a>
+        <a className="font-medium text-sm" href="">
+          Participantes
+        </a>
       </nav>
     </div>
   );
