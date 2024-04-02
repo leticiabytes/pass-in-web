b4155d80e3cbed7b4d921dad9e0505bcf1f013e7
Letícia Vieira <lvieira268@gmail.com>
2024-04-02 20:46:55 -0300

    feat: finish attendee-list component
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,3 +1,16 @@
+import { Search } from "lucide-react";
 export function AttendeeList() {
-  return <h1>Participantes</h1>;
+  return (
+    <div className="flex gap-3 items-center">
+      <div className="text-2xl font-bold">Participantes</div>
+      <div className="px-3 py-1.5 border border-white/10 rounded-lg text-sm w-72 flex items-center gap-3">
+        <Search className="size-4 text-emerald-300" />
+        <input
+          type="text"
+          placeholder="Buscar participante"
+          className="flex-1 outline-none bg-transparent"
+        />
+      </div>
+    </div>
+  );
 }
